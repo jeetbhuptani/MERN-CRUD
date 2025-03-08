e94d25b260f4c01a4283ab025305d70b06806756
Jeet Bhuptani <jeet.work.id@gmail.com>
2025-03-08 16:27:57 +0530

    implement product CRUD operations with Express and MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./product.model.js";
-
+import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express()
 
 app.use(express.json()); //middleware to allows us to accet JSON data in the req.data
 
-app.post("/api/products", (req, res) => {
-    const product = req.body;
-
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success: false, message: "Please provide all fields"});
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        newProduct.save();
-        res.status(201).json({sucess: true, data: newProduct});
-    } catch (error) {
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
