54295a19181a846387940f303220a170eae0fdf0
Jeet Bhuptani <jeet.work.id@gmail.com>
2025-03-11 19:31:45 +0530

    add Chakra UI icons and react-icons, enhance Navbar with color mode toggle

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,42 @@
+import { Button, Container, Flex, HStack, Text, useColorMode } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { FaRegPlusSquare } from "react-icons/fa";
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 const Navbar = () => {
+  const {colorMode, toggleColorMode} = useColorMode()
   return (
-    <div>Navbar</div>
+    <Container maxW={"1140px"} px={4}>
+      <Flex 
+        h={16} 
+        alignItems={"center"} 
+        justifyContent={"space-between"} 
+        flexDir={{
+          base:"column", 
+          sm:"row"
+        }}>
+          <Text
+            fontSize={{base:"22", sm:"28"}}
+            fontWeight={"bold"}
+            textTransform={"uppercase"}
+            textAlign={"center"}
+            bgGradient={"linear(to-r, cyan.400, blue.500)"}
+            bgClip={"text"}
+          >
+            <Link to={"/"}>Product Store 🛒</Link>
+          </Text>
+
+          <HStack spacing={2} alignItems={"center"}>
+            <Link to={"/create"}>
+              <Button>
+                <FaRegPlusSquare fontSize={20} />
+              </Button>
+            </Link>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <MoonIcon fontSize={20}/> : <SunIcon fontSize={20}/>}
+            </Button>
+          </HStack>
+      </Flex>
+    </Container>
   )
 }
 
